dc2e96b35271317a922a70724a713fe767960fcc
fumio68 <rgkk.5123@gmail.com>
2020-03-19 12:55:15 +0900

    add typeAnimation

--- js/script.js
+++ js/script.js
@@ -32,4 +32,5 @@ $(function () {
     return false;
   });
 
+
 });
---
+++ js/type.js
@@ -0,0 +1,6 @@
+ityped.init('#ityped', {
+  strings: ['思いを形にして伝えるエンジニアを目指して学習・就活中です！'],
+  typeSpeed: 100,
+  backSpeed: 80,
+  startDelay: 500
+});
